b1d93d6208f165059089abbfc608a008ef5f98ef
Ckalia11 <47508949+Ckalia11@users.noreply.github.com>
2019-04-16 16:05:28 -0400

    Update config.php

--- Files/load/config.php
+++ Files/load/config.php
@@ -3,4 +3,4 @@ define("DB_NAME", "Form");
 define("DB_USER", "root");
 define("DB_PASS", "");
 define("DB_HOST", "localhost");
-define("NONCE_SALT", "1Xdr4YlLHWkDvd3ZOo3ZzzBx9sP9tuUcEDUQVBh39Avn7BfWmKQGZJ0FjhV7");
+define("NONCE_SALT", "INSERT RANDOM STRING");
