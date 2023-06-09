6b877da5211e7273aac211fe797d314ba7b8cddf
Chetan <ckalia@uwaterloo.ca>
2023-06-09 16:58:49 -0400

    refactored config.php to work with local and remote db

--- Files/load/config.php
+++ Files/load/config.php
@@ -1,20 +1,21 @@
 <?php
-// define("DB_NAME", "Form");
-// define("DB_USER", "root");
-// define("DB_PASS", "");
-// define("DB_HOST", "localhost");
 define("NONCE_SALT", "INSERT RANDOM STRING");
 
-//Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+if ($_SERVER['HTTP_HOST'] == 'localhost') {
+    $DB_HOST   = "localhost";
+    $DB_USER   = "root";
+    $DB_PASS   = "";
+    $DB_NAME   = "Form";
+  }
 
+  else {
+    //Get Heroku ClearDB connection information
+    $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $DB_HOST = $cleardb_url["host"];
+    $DB_USER = $cleardb_url["user"];
+    $DB_PASS = $cleardb_url["pass"];
+    $DB_NAME = substr($cleardb_url["path"],1);
 
-$DB_HOST = $cleardb_url["host"];
-$DB_USER = $cleardb_url["user"];
-$DB_PASS = $cleardb_url["pass"];
-$DB_NAME = substr($cleardb_url["path"],1);
-echo '<h1>'.$DB_NAME.'</h1>';
-echo '<h1>'.$DB_USER.'</h1>';
-
-$active_group = 'default';
-$query_builder = TRUE;
+    $active_group = 'default';
+    $query_builder = TRUE;
+  }
