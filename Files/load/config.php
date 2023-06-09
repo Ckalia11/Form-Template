376a0a53343879d9b25e9dc264679591c8d8df51
Chetan <ckalia@uwaterloo.ca>
2023-06-09 16:53:05 -0400

    test1

--- Files/load/config.php
+++ Files/load/config.php
@@ -7,12 +7,14 @@ define("NONCE_SALT", "INSERT RANDOM STRING");
 
 //Get Heroku ClearDB connection information
 $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-echo '<h1>'.$cleardb_url.'</h1>';
 
 
 $DB_HOST = $cleardb_url["host"];
 $DB_USER = $cleardb_url["user"];
 $DB_PASS = $cleardb_url["pass"];
 $DB_NAME = substr($cleardb_url["path"],1);
+echo '<h1>'.$DB_NAME.'</h1>';
+echo '<h1>'.$DB_USER.'</h1>';
+
 $active_group = 'default';
 $query_builder = TRUE;
